12505145437b4fcb44aba1e344863257e2c58041
Emily Goodrich <emily.goodrich2751@mtec.stu.edu>
2023-10-23 09:40:08 -0600

    changed some css and in the middle of fixing the getGolfCourseDetails function to work

--- golfScoreApp.js
+++ golfScoreApp.js
@@ -9,29 +9,58 @@ async function getAvailableGolfCourses() {
   }
 
 //get golf course details
-function getGolfCourseDetails(golfCourseId) {
-    return fetch(
+async function getGolfCourseDetails(golfCourseId) {
+    return await fetch(
       `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`
       //, { mode: "no-cors" }
-    ).then(function (response) {
-      return response.json();
+    ).then(async function (response) {
+      return await response.json();
     });
   }
-//function for 
+
+//function for when the selection changes
   function onCourseChange() {
-    console.log('hello?')
     let x = 0;
   }
+
+  let courses = '';
+  let teeBoxes = '';
 //makes the selection work
   async function go() {
-    let courses = await getAvailableGolfCourses();
-
+    courses = await getAvailableGolfCourses(); 
     let courseOptionsHtml = '';
     courses.forEach((course) => {
      courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
     });
-    
+    console.log()
     document.getElementById('course-select').innerHTML = courseOptionsHtml;    
-  }
+    }
+  
+go();
+async function goAgain() {
+  teeBoxes = await getGolfCourseDetails(golfCourseId);
+  console.log(teeBoxes)
+}
+goAgain();
+let selectedCourse = '';
+  //finding the selected course 
+function submitCourseSelect() {
+   let x = document.getElementById('course-select').selectedIndex
+   selectedCourse = courses[x]
+   render();
+}
+function render() {
+  //rendering in the title
+  let courseTitle = '';
+  courseTitle += `<div>${selectedCourse.name}</div>`
+  document.getElementById('header').innerHTML = courseTitle
+  //render in the table with information saved in it's own array and stuff
+//   let teeBoxSelectHtml = ''
+// teeBoxes.forEach(function (teeBox, index) {
+//    teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
+//      teeBox.totalYards
+//    } yards</option>`
+// });
 
-  go();
+// document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
+}
