8673c29536750e4acab5e69a943e6adf906bf7a0
Emily Goodrich <emily.goodrich2751@mtec.stu.edu>
2023-10-26 09:56:21 -0600

    made it up to the point where im creating the first row of the table, although ill definitely go back and tweak some things

--- golfScoreApp.js
+++ golfScoreApp.js
@@ -23,7 +23,11 @@ async function getGolfCourseDetails(golfCourseId) {
   let teeBoxes = '';
   let holes = '';
   let holeChoices = [{id: 1, name: "9 Holes"}, {id: 2, name: "18 Holes"}]
+  let playerChoices = [{id: 1, name: "1 Player"}, {id: 2, name: "2 Players"}, {id: 3, name: "3 Players"}, {id: 4, name: "4 Players"}]
+  let playerAmount;
   let holeAmount;
+  let selectedTee;
+  let rowOne = ["Holes"];
 //makes the selection work
   async function go() {
     courses = await getAvailableGolfCourses(); 
@@ -36,55 +40,105 @@ async function getGolfCourseDetails(golfCourseId) {
     }
   
 go();
-
+//loads the data after a golf course is chosen 
 async function goAgain() {
   allOfDetails = await getGolfCourseDetails(selectedCourse.id);
   holes = allOfDetails.holes
   holeOptions()
   }
-function noClue() {
-  holes.length = holeAmount;
-  for (let i = 0; i < holeAmount; i++) {
-    teeBoxes = holes[i].teeBoxes
-  let teeBoxSelectHtml = ''
-    teeBoxes.forEach(function (teeBox, index) {
-    teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
-    teeBox.yards
-    } yards</option>`
-    document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
-  })}
-}
+
 
 let selectedCourse = '';
   //finding the selected course 
 function submitCourseSelect() {
    let x = document.getElementById('course-select').selectedIndex
    selectedCourse = courses[x]
+   if (holeAmount) {
+    //everything equals nothing??? or local storage ughhh
+   }
    goAgain();
    render();
 }
+//creating the buttons
 function holeOptions() {
-  let holeChoicesHTML = '';
+  var holeChoicesHTML = '';
   holeChoices.forEach((choice) => {
-    holeChoicesHTML += `<div class="holeOptionClass" onclick="holeNumberSelection(${choice.id})">${choice.name}</div>`
+    holeChoicesHTML += `<div class="holeOptionClass" id="holeOption${choice.id}" onclick="holeNumberSelection(${choice.id})">${choice.name}</div>`
   })
   document.getElementById('holeOptions').innerHTML = holeChoicesHTML;
-}
+  
+} 
+//making the 9 or 18 holes buttons work
 function holeNumberSelection(id) {
   if (id === 1) {
-    holeAmount = 9;
+    holeAmount = 10;
   } else if (id === 2) {
-    holeAmount = 18;
+    holeAmount = 19;
   } else {
     console.log('idk what went wrong, oopsies')
   }
-  document.getElementById('holeOptions').innerHTML = '';
+  for(let i=1; i < holeAmount; i++) {
+    rowOne[i] = i
+  }
+  console.log(rowOne)
+ document.getElementById(`holeOption${id}`).className = 'chosenOptionClass'
+ playerOptions()
+}
+//how many players buttons
+function playerOptions() {
+  var playerChoicesHTML = '';
+  playerChoices.forEach((players) => {
+    playerChoicesHTML += `<div class="playerOptionClass" id="player${players.id}" onclick="playerAmountSelection(${players.id})">${players.name}</div>`
+  })
+  document.getElementById('playerOptions').innerHTML = playerChoicesHTML;
+}
+//how many players rendering
+function playerAmountSelection(id) {
+  playerAmount = id;
+  //console.log(playerAmount + 3) it's a number!!!
+  document.getElementById(`player${id}`).className = 'chosenOptionClass'
   noClue()
 }
+//which tee button
+function noClue() {
+  holes.length = holeAmount;
+  for (let i = 0; i < holeAmount; i++) {
+    teeBoxes = holes[i].teeBoxes
+    var teeBoxSelectHtml = ''
+    teeBoxes.forEach(function (teeBox) {
+    teeBoxSelectHtml += `<option class="teeBoxOptionClass" id="teeBoxOption${teeBox.teeTypeId}" onclick="choosingTee(${teeBox.teeTypeId})">${teeBox.teeType.toUpperCase()}, ${
+    teeBox.yards
+    } yards</option>`
+    document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
+  })}
+}
+//tee button click 
+function choosingTee(teeTypeId) {
+document.getElementById('theOptions').innerHTML = '';
+selectedTee = teeBoxes.find(t => t.teeTypeId === teeTypeId);
+render();
+}
+
+//make restart button that appears with the players after choosing a hole so they can choose if they want to
+//and then make a confirm button after the yards have been chosen
+//bringing together the three introduction buttons 
+
+//maybe if i have time actually
+function userOptions() {
+
+}
+//rendering :thumb:
 function render() {
   //rendering in the title
   let courseTitle = '';
   courseTitle += `<div>${selectedCourse.name}</div>`
   document.getElementById('header').innerHTML = courseTitle
-  //render in the table with information saved in it's own array and stuf
-}//i don't really understand *how* the table is supposed to be created
+  //rendering the table with everything *ugh*
+  if (selectedTee) {
+    let table = '';
+    rowOne.forEach((holes) =>
+    {
+      table += `<div>${holes}</div>`
+    })
+  document.getElementById('theTable').innerHTML = table
+}}
